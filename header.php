a4f920699561fa398388f11098637c2ee9aed766
ITCharge <itcharge@163.com>
2021-06-21 18:02:07 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Sapphire
+ * @since Sapphire 1.0.0
+ */
+
+?>
+
+<!DOCTYPE html>
+<html class="no-js no-svg" <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	
+	<?php wp_head(); ?>
+</head>
