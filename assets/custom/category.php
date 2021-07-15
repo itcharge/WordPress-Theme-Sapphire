c47b95d55b6f68c34a746fd29a5862e084434f7c
ITCharge <itcharge@163.com>
2021-07-15 23:58:25 +0800

    自定义查找链接：归档、分类、标签
    自定义查找链接：归档、分类、标签

---
+++ assets/custom/category.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: 全部分类模板
+ *
+ * The template for displaying category pages
+ *
+ * @package Sapphire
+ * @since Sapphire 1.0
+ */
+?>
+
+
+<h1>测试</h1>
---
+++ assets/custom/tag.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: 全部标签模板
+ *
+ * The template for displaying all single posts
+ *
+ * @package Sapphire
+ * @since Sapphire 1.0
+ */
+?>
+
+<h1>测试</h1>
