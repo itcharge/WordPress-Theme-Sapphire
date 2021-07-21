eaf642ddd6ef554cbe7d0ecff4edf7fcfb177afa
ITCharge <itcharge@163.com>
2021-07-21 18:02:40 +0800

    Update category.php
    更新分类页面

--- inc/custom-pages/category.php
+++ inc/custom-pages/category.php
@@ -9,5 +9,39 @@
  */
 ?>
 
+<?php get_header(); ?>
+<div class="site-content">
+	<div id="left-box">
+		<div id="home-loop">
+			<div class="banner-box">
+				<div class="banner-header">
+					<h1 class="banner-title">分 类（<?php echo wp_count_terms('category'); ?>）</h1>
+				</div>
+			</div>
+			<div class="category-wrap">
+				<ul class="category-list">
+					<?php
+						$args = array(
+							'orderby'	=>	'count',
+							'order'		=>	'DESC',
+							'style'		=>	'list',
+							'title_li'	=>	'',
+							'depth'		=> 	2,
+							'show_count'=>	1,
+							'hide_empty'=>	1
+						);
+						wp_list_categories($args); 
+					?>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<?php get_sidebar(); ?>
+	<div class="clearfix"></div>
+</div>
 
-<h1>测试</h1>
+<a href="#" id="back-top">
+	<i class="icon-font icon-plane"></i>
+</a>
+
+<?php get_footer(); ?>
