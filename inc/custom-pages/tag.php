cb3f27af85c261dd091baee10e69c4df1c379e1f
ITCharge <itcharge@163.com>
2021-07-21 18:02:47 +0800

    Update tag.php
    更新标签页面

--- inc/custom-pages/tag.php
+++ inc/custom-pages/tag.php
@@ -8,5 +8,45 @@
  * @since Sapphire 1.0
  */
 ?>
+<?php get_header(); ?>
+<div class="site-content">
+	<div id="left-box">
+		<div id="home-loop">
+			<div class="banner-box">
+				<div class="banner-header">
+					<h1 class="banner-title">标 签（<?php echo wp_count_terms('post_tag'); ?>）</h1>
+				</div>
+			</div>
+			<div class="tag-wrap">
+				<div class="tag-entry">
+				<?php wp_tag_cloud('smallest=8&largest=20&number=30&order=DESC'); ?>
+				<?php
+					$args = array(
+						'orderby' 	=> 	'count', 
+						'order' 	=> 	'DESC', 
+						'show_count'=>	1,
+						'hide_empty'=> 	false
+					);
+					$tags_list = get_tags($args);
+					$count = 0; 
+					if ($tags_list) {
+						echo '<div class="side-info-index"><div class="side-tag tagcloud"><ul class="cat-tag-list">';
+						foreach($tags_list as $tag) {
+							$count++;
+							echo '<li class="cat-tag-list-item">';
+								echo '<a class="article-tag-link color'.rand(1, 6).'" href="'.get_tag_link($tag->term_id).'">'.$tag->name.'（'.$tag->count.'）</a>';
+							echo '</li>';
+							if( $count > 20 ) break;
+						}
+						echo '</ul></div><div class="clearfix"></div></div>';
+					}
+				?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php get_sidebar(); ?>
+	<div class="clearfix"></div>
+</div>
 
-<h1>测试</h1>
+<?php get_footer(); ?>
