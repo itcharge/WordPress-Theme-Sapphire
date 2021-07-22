35e89af9ffdb6653589750be6ddae38e4c384fec
ITCharge <itcharge@163.com>
2021-07-22 11:47:37 +0800

    Update 标签云颜色效果

--- inc/custom-configs/theme-colorcloud.php
+++ inc/custom-configs/theme-colorcloud.php
@@ -4,9 +4,9 @@ function colorCloud($text) {
 	return $text;
 }
 function rand_color() {
-	$red = sprintf('%02X', mt_rand(0x1B, 0xA4));
-	$green = sprintf('%02X', mt_rand(0xB9, 0xD8));
-	$blue = sprintf('%02X', mt_rand(0xF3, 0xFA));
+	$red = sprintf('%02X', mt_rand(0x2E, 0x34));
+	$green = sprintf('%02X', mt_rand(0x30, 0x49));
+	$blue = sprintf('%02X', mt_rand(0x32, 0x5e));
 	
 	return '#'.$red.$green.$blue;
 }
@@ -14,7 +14,7 @@ function colorCloudCallback($matches) {
 	$text = $matches[1];
 	$color = rand_color();
 	$pattern = '/style=(\'|\")(.*)(\'|\")/i';
-	$text = preg_replace($pattern, "style=\"color:{$color};$2;\"", $text);
+	$text = preg_replace($pattern, "style=\"color:$color; $2;\"", $text);
 	return "<a $text>";
 }
 add_filter('wp_tag_cloud', 'colorCloud', 1);
