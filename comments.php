3bbb61ce51f45d56b83d0ab80b08cc72d6673475
ITCharge <itcharge@163.com>
2021-07-23 14:15:19 +0800

    Update 评论模板

--- comments.php
+++ comments.php
@@ -20,69 +20,67 @@
 	<?php if ( comments_open() ) : ?>
 		<!-- 开启评论 -->
 		<header class="comment-header">
-			<h1 class="comment-title">发表评论</h1>
+			<h1 class="comment-title">评论（<?php echo comments_number();  ?>）</h1>
 		</header>
-	
+		
+		<?php if ( have_comments() ) : ?>
+			<div class="comment-list-container">
+				<ol class="comment-list">
+					<?php
+						$args = array(
+							'style'			=>	'ol',
+							'short_ping'	=>	'true',
+							'avater_size'	=>	48,
+							'type'			=>	'comment',
+							'callback'		=>	'sa_theme_comment',
+						);
+					 	wp_list_comments($args);
+					?>
+				</ol>
+			</div>
+		
+			<?php if ( get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
+				<nav class="comments-nav">
+					<?php paginate_comments_links(array('prev_next'=>true)); ?>
+				</nav>
+			<?php endif; // 评论分页 ?>
+		
+		<?php endif; // 评论列表 ?>
+		
 		<?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
+			<!-- 要求登录 -->
 			<div class="comment-alert-info">
 				<p>您必须 [<a href="<? echo wp_login_url( get_permalink() ); ?>">登录</a>] 才能发表评论.</p>
 			</div>
-		<?php else :  // 注册了未登录用户 ?>
-		
-			<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" class="comment-form" id="comment-form">
-				<?php if (is_user_logged_in()) : $current_user = wp_get_current_user(); ?>
-					<div class="comment-author-welcome">	
-						<?php echo get_avatar($current_user->user_email, 30); ?>
-						<a href="<?php echo get_edit_user_link(); ?>" class="links" data-no-instant><?php echo $user_identity; ?></a> 
-						<a href="<?php echo wp_logout_url(get_permalink()); ?>" class="links">退出</a>
-					</div>	
-				<?php endif;  // 登录用户 ?>	
-				
-				<?php if ( !is_user_logged_in() ) : ?>
-					<div class="comment-author-info" id="comment-author-info" <?php if ($comment_author) echo ' hidden'; ?>>
-						<div class="comment-author-username">
-							<label for="author">昵称<?php if ($req) echo "（必填）"; ?></label>
-							<input type="text" name="author" id="author" class="commenttext" value="<?php echo $comment_author; ?>" tabindex="1" <?php if ($req) echo "required"; ?>/>
+		<?php else :  // 非必须登录 ?>
+			<?php if(is_user_logged_in()) : $current_user = wp_get_current_user(); ?>
+				<!-- 登录用户 -->
+				<p class="warning-text" style="margin-bottom:10px">以<a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>身份登录&nbsp;|&nbsp;<a class="link-logout" href="<?php echo wp_logout_url(get_permalink()); ?>">注销 &raquo;</a></p>
+				<textarea class="form-control" rows="3" id="comment" onkeydown="if(event.ctrlKey&&event.keyCode==13){document.getElementById('submit').click();return false};" placeholder="欢迎留言，广告绕道~~" tabindex="1" name="comment"></textarea>
+			<?php else: ?>
+				<!-- 非登录用户 -->
+				<div class="comment-form-container" role="form">
+					<h2 class="comment-reply-title" id="comment-reply-title">
+						<?php comment_form_title( '', '回复给 %s' ); ?>
+						<small><?php cancel_comment_reply_link(); ?></small>
+					</h2>
+					<form action="<?php get_option('siteurl'); ?>/wp-comments-post.php" method="post" class="comment-form" id="comment-form">
+						<textarea class="form-control" rows="3" id="comment" onkeydown="if(event.ctrlKey&&event.keyCode==13){document.getElementById('submit').click();return false};" placeholder="欢迎留言，广告绕道~~" tabindex="1" name="comment"></textarea>
+						<div class="comment-form-info">
+							<label id="author_name" for="author">
+								<input class="form-control" id="author" type="text" tabindex="2" value="<?php echo $comment_author; ?>" name="author" placeholder="昵称 [必填]" required>
+							</label>
+							<label id="author_email" for="email">
+								<input class="form-control" id="email" type="text" tabindex="3" value="<?php echo $comment_author_email; ?>" name="email" placeholder="邮箱 [必填]" required>
+							</label>
 						</div>
-						<div class="comment-author-email">
-							<label for="email">邮箱<?php if ($req) echo "（必填）"; ?></label>
-							<input type="email" name="email" id="email" class="commenttext" value="<?php echo $comment_author_email; ?>" tabindex="2" <?php if ($req) echo "required"; ?>/>
-						</div>
-						<div class="comment-author-url">
-							<label for="url">网址</label>
-							<input type="text" name="url" id="url" class="commenttext" value="<?php echo $comment_author_url; ?>" tabindex="3" />
-						</div>
-					</div>
-				<?php endif; // 非登录用户 ?>
-				
-				<div class="comment-form-comment">
-					<textarea id="comment" name="comment" rows="3" tabindex="4" placeholder="<?php echo '欢迎留言'; ?>" required>
-					</textarea>
+						<div class="btn-group commentBtn" role="group">
+							<input name="submit" type="submit" id="submit" class="btn btn-sm btn-danger btn-block" tabindex="5" value="发表评论" /></div>
+						<?php comment_id_fields(); ?>
+					</form>
 				</div>
-				
-				<div class="comment-form-submit">
-					<?php comment_id_fields(); do_action('comment_form', $post->ID); ?>
-					<button id="submit" type="submit" tabindex="5">提交评论</button>
-				</div>
-			</form>
-		
-		<?php endif;  // 非必须登录 ?>
-
-	<?php endif; // 打开评论 ?>
-	
-	<?php if ( have_comments() ) : ?>
-		<!-- 评论列表 -->	
-		<ol class="comment-list">
-			<?php wp_list_comments(array('callback' => 'sa_theme_comment')); ?>
-		</ol>
-	
-		<!-- 评论翻页 -->
-	    <?php if( get_comment_pages_count() > 1 && get_option('page_comments') ) : ?>
-			<nav class="comment-navi">
-				<?php paginate_comments_links('prev_text=«&next_text=»'); ?>
-	    	</nav>
-	    <?php endif; ?> 
-	<?php endif; ?>
-	
+			<?php endif; ?>
+		<?php endif; ?>
+	<?php endif;  // 开启评论 ?>
 	<div class="clearfix"></div>
 </div>
--- inc/custom-configs/theme-comment.php
+++ inc/custom-configs/theme-comment.php
@@ -1,146 +1,34 @@
 <?php
-/**
- * 自定义评论模板
- *
- * @package Sapphire
- * @since Sapphire 1.0
- */
+
+function sa_theme_comment($comment, $args, $depth) {
+	$GLOBALS['comment'] = $comment; 
 ?>
-
-<?php
-
-function sa_theme_comment($comment, $args='', $depth='') {
-	$GLOBALS['comment'] = $comment;
-	
-	$is_parent = true;
-	
-	if ($args) {
-		extract($args, EXTR_SKIP);
-		
-		$comorder = get_option('comment_order');
-		if ($comorder == 'asc') {
-			global $commentcount;
-			if (!$commentcount) {
-				$page = (int) get_query_var('cpage');
-				if ($page > 0) {
-					$page--;
-				} 
-				$cpp = (int) get_option('comments_per_page');
-				$commentcount = $cpp * $page;
-			}
-		} else {
-			global $commentcount, $wpdb, $post;
-			if (!$commentcount) {
-				$comments = $wpdb->get_results("SELECT * FROM $wpdb->$comments WHERE comment_post_ID = $post->ID AND comment_type = '' AND comment_approved = '1' AND !comment_parent");
-				$cnt = count($comments);
-				$page = (int) get_query_var('cpage');
-				$cpp = (int) get_option('comments_per_page');
-				if ($page == 0 || ceil($cnt/$cpp) == 1 || ($page > 1 && $page == ceil($cnt/$cpp))) {
-					$commentcount = $cnt + 1;
-				} else {
-					$commentcount = $cpp * $page + 1;
-				}
-			}
-		}
-		if ( empty($args['has_children'])) {
-			$isParent = false;
-		}
-	}  else {
-		if ($depth > 0) {
-			$isParent = false;
-			echo '<ul class="children">';
-		}
-	}
-?>
-
-<li <?php comment_class( $isParent? 'parent': ''); ?> id="comment-<?php comment_ID() ?>" data-no-instant>
-	<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
-		<div class="comment-avatar-area">
-			<?php echo get_avatar($comment->comment_author_email, 50, '', $comment->comment_author); ?>
-		</div>
-		
-		<div class="comment-content-area">
-			<div class="comment-content-user">
-				<span class="comment-auth"><?php comment_author_link(); ?></span>
-<!-- 				<?php mk_author_level($comment); ?> -->
-			</div>
-			
-			<div class="comment-content-text">
-				<?php comment_text(); ?>
-				<?php if ( $comment->comment_approved == '0' ) : ?>
-				<p class="comment-awaiting-moderation">
-					<i class="fa fa-lock"></i> 该评论正在审核中...
-				</p>
+	<li class="comment-list-li" id="comment-list-li-<?php comment_ID(); ?>">
+   		<div class="media">
+   			<div class="media-left">
+        		<?php 
+					if (function_exists('get_avatar') && get_option('show_avatars')) { 
+						echo get_avatar($comment, 48); 
+					} 
+				?>
+   			</div>
+   			<div class="media-body">
+   				<?php printf(__('<p class="author_name">%s</p>'), get_comment_author_link()); ?>
+		        <?php if ($comment->comment_approved == '0') : ?>
+		            <em>评论等待审核...</em><br/>
 				<?php endif; ?>
-			</div>
-			
-			<?php if($args) { ?>
-			<div class="comment-content-info" data-no-instant>
-				<?php if(!$comment->comment_parent) { ?>
-				<span>
-					<a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>">
-					<?php
-						if($comorder == 'asc') {
-							$commentcount++;    // 在页面顶部显示 旧的 评论
-						} else {
-							$commentcount--;    // 在页面顶部显示 新的 评论
-						}
-						switch ($commentcount) {
-							case 1: echo '沙发'; break;
-							case 2: echo '板凳'; break;
-							case 3: echo '地板'; break;
-							default: printf('#%1$s', $commentcount);
-						}
-					?>
-					</a>
-				</span>
-				<?php } ?>
-				
-				<span>
-				<?php 
-				printf( __('%1$s at %2$s'), get_comment_date( 'Y-m-d' ),  get_comment_time('H:i') );
-				?>
-				</span>
-				
-				<?php
-				if ( is_user_logged_in() ) {
-					echo '
-					<span>
-						<a class="comment-delete-link" id="delete-'.$comment->comment_ID .'" 
-						  href="'.get_bloginfo('url').'/wp-admin/comment.php?action=trash&c='.$comment->comment_ID.'#wpbody-content">
-							删除
-						</a>
-					</span>';
-					
-					edit_comment_link('编辑', '<span>', '</span>');
-				}
-				?>
-				
-				<span><?php 
-				if(comments_open()) {
-					$link = sprintf(
-						"<a rel='nofollow' class='comment-reply-link' href='%s' onclick='replyComments(%s); return false;'>回复</a>",
-						esc_url(
-							add_query_arg(
-								array(
-									'replytocom'      => $comment->comment_ID,
-									'unapproved'      => false,
-									'moderation-hash' => false,
-								)
-							)
-						) . '#respond',
-						$comment->comment_ID
-					);
-					echo $link;
-				}
-				?></span>
-			</div>  <!-- comment-content-info -->
-			<?php } ?>
-		</div>      <!-- comment-content-area -->
-	</div>      <!-- .comment-body -->
-		
-	<?php
-
-	if(!$args && !$isParent) echo '</ul>';
-}
+				<?php comment_text(); ?>
+   			</div>
+   		</div>
+   		<div class="comment-metadata">
+   			<span class="comment-pub-time">
+   				<?php echo get_comment_time('Y-m-d H:i'); ?>
+   			</span>
+   			<span class="comment-btn-reply">
+ 				<i class="fa fa-reply"></i> <?php comment_reply_link(array_merge( $args, array('reply_text' => '回复','depth' => $depth, 'max_depth' => $args['max_depth']))) ?> 
+   			</span>
+   		</div>
+	</li>
+<?php
+}	
 ?>
